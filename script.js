5926efbfb4f9185849e64989cecd0b8469d5d0a5
XlolekNaX <91343698+XlolekNaX@users.noreply.github.com>
2022-10-27 18:50:28 +0200

    Add files via upload
    gf

---
+++ script.js
@@ -0,0 +1,6 @@
+var filtrWszystkie = document.getElementsByClassName('wszystkie');
+var filtrNatura = document.getElementsByClassName('natura');
+var filtrBudowle = document.getElementsByClassName('budowle');
+var filtrZwierzeta = document.getElementsByClassName('zwierzeta');
+
+
